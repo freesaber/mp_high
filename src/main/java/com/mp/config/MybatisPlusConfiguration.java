1b01d5c619f8b7a5567b99d56a5a575a3a3ad4ea
freesaber <free.saber@qq.com>
2022-10-08 14:29:53 +0800

    多租户

--- src/main/java/com/mp/config/MybatisPlusConfiguration.java
+++ src/main/java/com/mp/config/MybatisPlusConfiguration.java
@@ -3,6 +3,7 @@ package com.mp.config;
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +17,15 @@ public class MybatisPlusConfiguration {
     @Bean
     public OptimisticLockerInterceptor optimisticLockerInterceptor(){
         return new OptimisticLockerInterceptor();
+    }
+
+    /**
+     * 3.4.3.4版本以后多租户用的是插件，已经废弃了这种方法而且会报错。
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+
+        return paginationInterceptor;
     }
 }
